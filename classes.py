affc6c3f4858c759a8b8f7c5ef5127609d639752
Sylvester Worrell <64230787+iamsly3@users.noreply.github.com>
2021-06-09 17:10:05 -0400

    second commit
    updated and corrected code

--- classes.py
+++ classes.py
@@ -1,63 +1,69 @@
 class Category:
 
     def __init__(self, category, amount):
-       # self.food = food
-        self.category = category
-       # self.carExpense = carExpense
         self.amount = amount
+        self.category = category
+
 
     #methods
     def deposit(self, amount):
         self.amount += amount
-        return "{} was deposited in your {} account for bread and milk".format(amount, self.category)
+        return "{} was deposited in your {} account".format(amount, self.category)
 
-    def budget_balance(self):
-        return "your available balance is {} in your account".format(self.amount)
     
         
-    def check_balance(self, amount):
-        self.amount <= amount
-        return "{} is the current balance in your {} account".format(amount, self.category)
-
-   # def budget_balance(self):
-        #return "your available balance is {} for food".format(self.amount)
-    
+    def check_balance(self):
+        return "{} is the current balance in your {} account".format(self.amount, self.category)
 
     
-
     def withdrawl(self, amount):
-        self.amount -= amount
-        return "{} was withdrawn from your {} account to pay your phone bill".format(amount, self.category)
-
-    
+        if self.check_balance():
+            self.amount -= amount
+            return "{} was withdrawn from your {} account".format(amount, self.category)
+        else:
+            return "Transaction fail..insufficient funds"
         
 
-    def transfer(self, amount):
-        self.amount - amount
-        return "{} was transfer from your {} account to your carExpense account to pay your car note".format(amount, self.category)
+    def transfer(self, amount, category):
+        if self.amount < amount:
+            return "Transaction fail, Insufficient funds"
+        
+        else:
+            self.amount -= amount
+            category.amount += amount
+
+            print("Transfer successful")
 
 
+            return "{} was transfer from your {} account to your carExpense account to pay your car note".format(amount, self.category)
 
-food = Category("Food", 1000)
+
+food = Category("food", 1000)
 clothing = Category("clothing", 500)
 carExpense = Category("carExpense", 200)
-
+furniture = Category("furniture", 2000 )
+Lodging = Category("Rent", 1000)
 
 print(food.deposit(10))
-print(food.budget_balance())
+print(food.withdrawl(20))
+print(food.check_balance())
 
-print(clothing.check_balance(500))
-print(clothing.budget_balance())
+print(clothing.transfer(50, food))
+print(clothing.deposit(5))
+print(clothing.check_balance())
 
+print(food.transfer(200, carExpense))
+print(carExpense.withdrawl(30))
+print(carExpense.check_balance(),)
 
+print(furniture.deposit(50))
+print(furniture.withdrawl(500))
+print(furniture.check_balance())
 
-print(food.withdrawl(100))
-print(food.budget_balance())
+print(Lodging.deposit(200))
+print(Lodging.transfer(100,furniture))
+print(Lodging.withdrawl(1500))
+print(Lodging.check_balance())
 
-print(food.transfer(200))
-print(carExpense.check_balance(200))
-print(carExpense.budget_balance())
-print(clothing.budget_balance())
-
-
+print(food.check_balance())
 
